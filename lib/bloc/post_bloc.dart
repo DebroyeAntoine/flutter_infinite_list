5bafe566b5781cb58226c27d88ba7684a0e9a6b5
Antoine Debroye <ant.debroye@gmail.com>
2022-10-08 21:45:16 +0200

    First implementation of the post bloc, non functionnal for the moment

---
+++ lib/bloc/post_bloc.dart
@@ -0,0 +1,40 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter_infinite_list/models/post.dart';
+
+part 'post_event.dart';
+part 'post_state.dart';
+
+
+class PostBloc extends Bloc<PostEvent, PostState> {
+  PostBloc({required this.httpClient}) : super(const PostState()) {
+    on<PostFetched>(_onPostFetched);
+  }
+  Future<void> _onPostFetched(PostFetched event, Emitter<PostState> emit) async {
+    if (state.hasReachedMax) return;
+    try {
+      if (state.status == PostStatus.initial) {
+        final posts = await _fetchPosts();
+        return emit(state.copyWith(
+          status: PostStatus.success,
+          posts: posts,
+          hasReachedMax: false,
+        ));
+      }
+      final posts = await _fetchPosts(state.posts.length);
+      emit(posts.isEmpty
+          ? state.copyWith(hasReachedMax: true)
+          : state.copyWith(
+        status: PostStatus.success,
+        posts: List.of(state.posts)..addAll(posts),
+        hasReachedMax: false,
+      ));
+    } catch (_) {
+      emit(state.copyWith(status: PostStatus.failure));
+    }
+  }
+
+  final http.Client httpClient;
+}
